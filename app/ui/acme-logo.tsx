00069453b32ed96c0bee0312278bbaf2ed8bd708
Siumafua <siumafua.moala@gmail.com>
2024-11-06 21:29:55 +1200

    initial database

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,14 @@
+import { GlobeAltIcon,UsersIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <UsersIcon />
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">HRManager</p>
+    </div>
+  );
+}
